0b141772a54eb1b8c05d1df502e2737d850170f7
AntiEntropy <67179171+Anti-Entrophic@users.noreply.github.com>
2023-03-24 12:45:09 +0800

    Update data_utils.py

--- FastSpeech2/data_utils.py
+++ FastSpeech2/data_utils.py
@@ -12,7 +12,7 @@ def load_wav_to_torch(full_path):
     sampling_rate, data = read(full_path)
     return torch.FloatTensor(data.astype(np.float32)), sampling_rate
 
- def load_filepaths_and_PPG(filename, split="|"):
+def load_filepaths_and_PPG(filename, split="|"):
     with open(filename, encoding='utf-8') as f:
         filepaths_and_PPG = [line.strip().split(split) for line in f]
     return filepaths_and_PPG
