e82c527470c55ea8f1e35768676b71b602bda343
AntiEntropy <67179171+Anti-Entrophic@users.noreply.github.com>
2023-04-05 17:07:36 +0800

    Update evaluate.py

--- FastSpeech2/evaluate.py
+++ FastSpeech2/evaluate.py
@@ -10,14 +10,14 @@ from utils.model import get_model, get_vocoder
 from utils.tools import to_device, log, synth_one_sample
 from model import FastSpeech2Loss
 from dataset import Dataset
-
+from data_utils import PPG_MelLoader_test, PPGMelCollate, to_gpu
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def evaluate(model, step, configs, logger=None, vocoder=None):
+def evaluate(model, step, configs, hparams, logger=None, vocoder=None):
     preprocess_config, model_config, train_config = configs
-
+    '''
     # Get dataset
     dataset = Dataset(
         "val.txt", preprocess_config, train_config, sort=False, drop_last=False
@@ -29,6 +29,17 @@ def evaluate(model, step, configs, logger=None, vocoder=None):
         shuffle=False,
         collate_fn=dataset.collate_fn,
     )
+    '''
+    collate_fnn = PPGMelCollate(hparams.n_frames_per_step)
+    dataset = PPG_MelLoader_test(hparams.training_files, hparams)
+    batch_size = train_config["optimizer"]["batch_size"]
+
+    loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=collate_fnn,
+    )
 
     # Get loss function
     Loss = FastSpeech2Loss(preprocess_config, model_config).to(device)
@@ -36,27 +47,42 @@ def evaluate(model, step, configs, logger=None, vocoder=None):
     # Evaluation
     loss_sums = [0 for _ in range(6)]
     for batchs in loader:
-        for batch in batchs:
-            batch = to_device(batch, device)
-            with torch.no_grad():
-                # Forward
-                output = model(*(batch[2:]))
+        PPG = batchs[0]
+        input_lengths = batchs[1] 
+        mel_padded = batchs[2]
+        gate_padded = batchs[3]
+        output_lengths = batchs[4]
 
-                # Cal Loss
-                losses = Loss(batch, output)
+        PPG = to_gpu(PPG).float()
+        input_lengths = to_gpu(input_lengths).long()
+        max_len = torch.max(input_lengths.data).item()
+        mel_padded = to_gpu(mel_padded).float()
+        gate_padded = to_gpu(gate_padded).float()
+        output_lengths = to_gpu(output_lengths).long()
 
-                for i in range(len(losses)):
-                    loss_sums[i] += losses[i].item() * len(batch[0])
+        # Forward
+        output = model(PPG, input_lengths, mel_padded, max_len, output_lengths)
+        mel_predictions = output[0]
+        postnet_mel_predictions = output[1]
+        mel_masks = output[2]
+                
+        # Cal Loss
+        losses = Loss(PPG, input_lengths, mel_padded, max_len, output_lengths, 
+            mel_predictions, postnet_mel_predictions, mel_masks)
+        total_loss = losses[0]
+
+        for i in range(len(losses)):
+            loss_sums[i] += losses[i].item() * len(batchs[0])
 
     loss_means = [loss_sum / len(dataset) for loss_sum in loss_sums]
 
-    message = "Validation Step {}, Total Loss: {:.4f}, Mel Loss: {:.4f}, Mel PostNet Loss: {:.4f}, Pitch Loss: {:.4f}, Energy Loss: {:.4f}, Duration Loss: {:.4f}".format(
+    message = "Validation Step {}, Total Loss: {:.4f}, Mel Loss: {:.4f}, Mel PostNet Loss: {:.4f}".format(
         *([step] + [l for l in loss_means])
     )
-
+    '''
     if logger is not None:
         fig, wav_reconstruction, wav_prediction, tag = synth_one_sample(
-            batch,
+            batchs,
             output,
             vocoder,
             model_config,
@@ -82,7 +108,7 @@ def evaluate(model, step, configs, logger=None, vocoder=None):
             sampling_rate=sampling_rate,
             tag="Validation/step_{}_{}_synthesized".format(step, tag),
         )
-
+    '''
     return message
 
 
